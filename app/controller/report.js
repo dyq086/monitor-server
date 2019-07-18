31d4f080a5a0686277e850c4961000f6053d6306
yongqing <490844594@qq.com>
2019-07-18 10:30:26 +0800

    收集接口

--- app/controller/report.js
+++ app/controller/report.js
@@ -2,7 +2,7 @@
  * @Author: yongqing
  * @Date:   2019-07-15 11:19:16
  * @Last Modified by:   yongqing
- * @Last Modified time: 2019-07-15 18:25:21
+ * @Last Modified time: 2019-07-16 18:37:30
  */
 
 
@@ -11,27 +11,68 @@ const Controller = require('egg').Controller;
 class ReportController extends Controller {
     async save() {
         const { ctx } = this;
+
         let {
+            name,
+            type,
             title,
-            error_message,
+            message,
             script_uri,
             line_no,
             column_no,
             stack,
             resource_url,
             category,
+            referrer,
+            origin,
+            mobile,
+            userAgent,
+            os,
+            version,
+            cookies,
+            winWith,
+            windHeight,
+            language,
+            cid,
+            cip,
+            cname,
+            time,
+            env,
+            response,
+            request,
+
 
         } = ctx.request.body;
 
         const data = {
+            name,
+            type,
             title,
-            error_message,
+            message,
             script_uri,
             line_no,
             column_no,
             stack,
             resource_url,
             category,
+            referrer,
+            origin,
+            mobile,
+            user_agent: userAgent,
+            os,
+            version,
+            cookies,
+            win_with: winWith,
+            wind_height: windHeight,
+            language,
+            cid,
+            cip,
+            cname,
+            time,
+            env,
+            response: JSON.stringify(response),
+            request: JSON.stringify(request),
+            status: 0
         }
 
 
--- app/model/report.js
+++ app/model/report.js
@@ -2,7 +2,7 @@
  * @Author: yongqing
  * @Date:   2019-07-15 17:54:05
  * @Last Modified by:   yongqing
- * @Last Modified time: 2019-07-15 18:31:50
+ * @Last Modified time: 2019-07-16 18:38:16
  */
 
 'use strict';
@@ -18,16 +18,36 @@ module.exports = app => {
 
     const Report = app.model.define('log_error_msg', {
         id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+        name: STRING(30),
+        type: STRING(30),
         title: STRING(30),
-        error_message: STRING(500),
+        message: STRING(800),
         script_uri: STRING(30),
         line_no: INTEGER,
         column_no: INTEGER,
         stack: STRING(200),
         resource_url: STRING(30),
         category: STRING(20),
+        referrer: STRING(100),
+        origin: STRING(100),
+        mobile: STRING(50),
+        user_agent: STRING(500),
+        os: STRING(50),
+        version: STRING(50),
+        cookies: STRING(800),
+        win_with: INTEGER,
+        wind_height: INTEGER,
+        language: STRING(50),
+        cid:STRING(30),
+        cip:STRING(30),
+        cname:STRING(30),
+        time: INTEGER(100),
+        env: STRING(50),
+        response: STRING(500),
+        request: STRING(500),
         created_at: DATE,
-        updated_at: DATE
+        updated_at: DATE,
+        status: INTEGER
     }, {
         tableName: 'log_error_msg'
     });
